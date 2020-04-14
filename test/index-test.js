b988780e672c8c57b16773609b0e5d65fe63308c
Jojo Concepcion <hsconcepcion@gmail.com>
2020-04-14 04:16:04 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn("I can warn you!")', done => {
+  it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
